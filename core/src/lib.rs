a269af02429e68b8855f661e3273c45eb61e63a7
Robert Habermeier <rphmeier@gmail.com>
2024-03-20 19:36:44 -0500

    docs: add more

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,6 +3,19 @@
 //! This crate defines the schema and basic operations over the merkle trie in a backend-agnostic manner.
 //! 
 //! Nothing within this crate relies on the standard library.
+//!
+//! ## Schema
+//!
+//! This crate defines a binary merkle trie, generalized over a 256 bit hash function. All keys have 256 bits.
+//!
+//! There are two kinds of  nodes: [`Internal`] nodes, which each have two children, 
+//! and [`Leaf`] nodes which store value hashes.
+//! The trie itself does not store values. Sub-tries which contain no values are referenced with 
+//! the placeholder [`EMPTY_SUBTREE`] hash. All nodes can be encoded as 512 bit values.
+//!
+//! Node hashes are domain-separated: the most significant bit is reserved
+//! as an indication of the type of node. A node hash with an MSB of 0 refers to an internal node, 
+//! and a node hash with an MSB of 1 refers to a leaf node.
 
 #![no_std]
 
@@ -21,10 +34,10 @@ pub type ValueHash = [u8; 32];
 /// whether the node is a leaf or internal node.
 pub type NodeEncoding = [u8; 64];
 
-/// Denotes an empty sub-tree beyond this point. Concretely, when this appears within
+/// Special node hash value denoting an empty sub-tree. Concretely, when this appears within
 /// an internal node, it implies that no keys beginning with the path to the internal node plus the child's bit
-/// will have values within the trie.
-pub const NODE_EMPTY: NodeHash = [0u8; 32];
+/// will have values within the trie. This value may appear within an internal node at any height, including the root.
+pub const EMPTY_SUBTREE: NodeHash = [0u8; 32];
 
 /// Whether the node hash indicates the node is a leaf.
 pub fn is_leaf(hash: &NodeHash) -> bool {
@@ -36,9 +49,9 @@ pub fn is_internal(hash: &NodeHash) -> bool {
     hash[0] >> 7 == 0 && !is_empty(&hash)
 }
 
-/// Whether the node holds the special `NODE_EMPTY` value.
+/// Whether the node holds the special `EMPTY_SUBTREE` value.
 pub fn is_empty(hash: &NodeHash) -> bool {
-    hash == &NODE_EMPTY
+    hash == &EMPTY_SUBTREE
 }
 
 /// An internal (branch) node. This carries a left and right child.
@@ -78,6 +91,14 @@ impl Leaf {
         node[32..64].copy_from_slice(&self.value_hash[..]);
         node
     }
+}
+
+/// Either kind of node.
+pub enum Node {
+    /// An internal node.
+    Internal(Internal),
+    /// A leaf node.
+    Leaf(Leaf),
 }
 
 /// A trie node hash function specialized for 64 bytes of data.
