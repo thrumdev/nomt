e490d622fdbf6b38f99e5e4cf61578c2b126f99d
Robert Habermeier <rphmeier@gmail.com>
2024-03-21 14:28:55 -0500

    chore: more rigorous representation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,26 +1,34 @@
 //! Core operations and types within the Nearly Optimal Merkle Trie.
 //!
-//! This crate defines the schema and basic operations over the merkle trie in a backend-agnostic manner.
+//! This crate defines the schema and basic operations over the merkle trie in a backend-agnostic
+//! manner.
 //!
 //! Nothing within this crate relies on the standard library.
 //!
 //! ## Schema
 //!
-//! This crate defines a binary merkle trie, generalized over a 256 bit hash function. All keys have 256 bits.
+//! This crate defines a binary merkle trie, generalized over a 256 bit hash function. All lookup
+//! paths in the trie are 256 bits.
 //!
-//! There are two kinds of  nodes: [`Internal`] nodes, which each have two children, 
-//! and [`Leaf`] nodes which store value hashes.
-//! The trie itself does not store values. Sub-tries which contain no values are referenced with 
-//! the placeholder [`EMPTY_SUBTREE`] hash. All nodes can be encoded as 512 bit values.
+//! All nodes are 256 bits. There are three kinds of nodes.
+//!   1. Internal nodes, which each have two children. The value of an internal node is
+//!       given by hashing the concatenation of the two child nodes and setting the MSB to 0.
+//!   2. Leaf nodes, which have zero children. The value of a leaf node is given by hashing
+//!       the concatenation of the 256-bit lookup path and the hash of the value stored at the leaf,
+//!       and setting the MSB to 1.
+//!   3. [`TERMINATOR`] nodes, which have the special value of all 0s. These nodes have no children
+//!      and serve as a stand-in for an empty sub-trie at any height. Terminator nodes enable the
+//!      trie to be tractably represented.
 //!
-//! Node hashes are domain-separated: the most significant bit is reserved
-//! as an indication of the type of node. A node hash with an MSB of 0 refers to an internal node, 
-//! and a node hash with an MSB of 1 refers to a leaf node.
+//! All node preimages are 512 bits.
 
 #![no_std]
 
-/// The hash of a node. In this schema, it is always 256 bits.
-pub type NodeHash = [u8; 32];
+/// A node in the binary trie. In this schema, it is always 256 bits and is the hash of an
+/// underlying structure, represented as [`NodePreimage`].
+///
+/// The first bit of the [`Node`] is used to indicate the kind of node
+pub type Node = [u8; 32];
 
 /// The path to a key. All paths have a 256 bit fixed length.
 pub type KeyPath = [u8; 32];
@@ -28,43 +36,45 @@ pub type KeyPath = [u8; 32];
 /// The hash of a value. In this schema, it is always 256 bits.
 pub type ValueHash = [u8; 32];
 
-/// The encoded state of a node. In this schema, it is always 512 bits.
+/// The preimage of a node. In this schema, it is always 512 bits.
 ///
 /// Note that this encoding itself does not contain information about
 /// whether the node is a leaf or internal node.
-pub type NodeEncoding = [u8; 64];
+pub type NodePreimage = [u8; 64];
 
-/// Special node hash value denoting an empty sub-tree. Concretely, when this appears within
-/// an internal node, it implies that no keys beginning with the path to the internal node plus the child's bit
-/// will have values within the trie. This value may appear within an internal node at any height, including the root.
-pub const EMPTY_SUBTREE: NodeHash = [0u8; 32];
+/// The terminator hash is a special node hash value denoting an empty sub-tree.
+/// Concretely, when this appears at a given location in the trie,
+/// it implies that no key with a path beginning with the location has a value.
+///
+/// This value may appear at any height.
+pub const TERMINATOR: Node = [0u8; 32];
 
 /// Whether the node hash indicates the node is a leaf.
-pub fn is_leaf(hash: &NodeHash) -> bool {
+pub fn is_leaf(hash: &Node) -> bool {
     hash[0] >> 7 == 1
 }
 
 /// Whether the node hash indicates the node is an internal node.
-pub fn is_internal(hash: &NodeHash) -> bool {
-    hash[0] >> 7 == 0 && !is_empty(&hash)
+pub fn is_internal(hash: &Node) -> bool {
+    hash[0] >> 7 == 0 && !is_terminator(&hash)
 }
 
 /// Whether the node holds the special `EMPTY_SUBTREE` value.
-pub fn is_empty(hash: &NodeHash) -> bool {
-    hash == &EMPTY_SUBTREE
+pub fn is_terminator(hash: &Node) -> bool {
+    hash == &TERMINATOR
 }
 
-/// An internal (branch) node. This carries a left and right child.
-pub struct Internal {
+/// The data of an internal (branch) node.
+pub struct InternalData {
     /// The hash of the left child of this node.
-    pub left: NodeHash,
+    pub left: Node,
     /// The hash of the right child of this node.
-    pub right: NodeHash,
+    pub right: Node,
 }
 
-impl Internal {
+impl InternalData {
     /// Encode the internal node.
-    pub fn encode(&self) -> NodeEncoding {
+    pub fn encode(&self) -> NodePreimage {
         let mut node = [0u8; 64];
         node[0..32].copy_from_slice(&self.left[..]);
         node[32..64].copy_from_slice(&self.right[..]);
@@ -72,20 +82,20 @@ impl Internal {
     }
 }
 
-/// A leaf node carrying a value.
-pub struct Leaf {
+/// The data of a leaf node.
+pub struct LeafData {
     /// The total path to this value within the trie.
     ///
-    /// The actual location of this node may be anywhere along this path, depending on the other data
-    /// within the trie.
+    /// The actual location of this node may be anywhere along this path, depending on the other
+    /// data within the trie.
     pub key_path: KeyPath,
     /// The hash of the value carried in this leaf.
     pub value_hash: ValueHash,
 }
 
-impl Leaf {
+impl LeafData {
     /// Encode the leaf node.
-    pub fn encode(&self) -> NodeEncoding {
+    pub fn encode(&self) -> NodePreimage {
         let mut node = [0u8; 64];
         node[0..32].copy_from_slice(&self.key_path[..]);
         node[32..64].copy_from_slice(&self.value_hash[..]);
@@ -94,23 +104,23 @@ impl Leaf {
 }
 
 /// Either kind of node.
-pub enum Node {
+pub enum NodeData {
     /// An internal node.
-    Internal(Internal),
+    Internal(InternalData),
     /// A leaf node.
-    Leaf(Leaf),
+    Leaf(LeafData),
 }
 
 /// A trie node hash function specialized for 64 bytes of data.
 pub trait NodeHasher {
     /// Hash a node, encoded as exactly 64 bytes of data. This should not
     /// domain-separate the hash.
-    fn hash_node(data: &[u8; 64]) -> NodeHash;
+    fn hash_node(data: &NodePreimage) -> [u8; 32];
 }
 
 pub trait NodeHasherExt: NodeHasher {
     /// Hash an internal node. This returns a domain-separated hash.
-    fn hash_internal(internal: &Internal) -> NodeHash {
+    fn hash_internal(internal: &InternalData) -> Node {
         let data = internal.encode();
         let mut hash = Self::hash_node(&data);
 
@@ -120,7 +130,7 @@ pub trait NodeHasherExt: NodeHasher {
     }
 
     /// Hash a leaf node. This returns a domain-separated hash.
-    fn hash_leaf(leaf: &Leaf) -> NodeHash {
+    fn hash_leaf(leaf: &LeafData) -> Node {
         let data = leaf.encode();
         let mut hash = Self::hash_node(&data);
 
