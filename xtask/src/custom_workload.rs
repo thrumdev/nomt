eb902fd55a00159e0af71861ca1242d6f25d26eb
gabriele-0201 <gabriele.miotti2@gmail.com>
2024-04-12 12:24:39 +0200

    bench: remove workload trait

--- xtask/src/custom_workload.rs
+++ xtask/src/custom_workload.rs
@@ -1,86 +1,64 @@
-use crate::{
-    backend::{Action, Db},
-    timer::Timer,
-    workload::Workload,
-};
+use crate::{backend::Action, workload::Workload};
 use anyhow::Result;
 
-#[derive(Debug, Clone)]
-#[allow(unused)]
-pub struct CustomWorkload {
-    init_actions: Vec<Action>,
-    run_actions: Vec<Action>,
-}
+// The custom workload will follow these rules:
+// 1. Reads and writes are randomly and uniformly distributed over the key space.
+// 2. Deletes and updates will be performed on already present keys.
+// 3. additional_initial_capacity represents the amount of items already present
+//     in the DB in addition to the ones required to perform deletes and updates.
+// 4. Size represents the total number of operations, where reads, writes, etc
+//     are numbers that need to sum to 100 and represent a percentage of the total size.
+pub fn new_custom_workload(
+    reads: u8,
+    writes: u8,
+    deletes: u8,
+    updates: u8,
+    size: u64,
+    additional_initial_capacity: u64,
+) -> Result<Workload> {
+    if reads + writes + deletes + updates != 100 {
+        anyhow::bail!("Operations (reads, writes, deletes, updates) must sum to 100");
+    }
 
-impl CustomWorkload {
-    // The custom workload will follow these rules:
-    // 1. Reads and writes are randomly and uniformly distributed over the key space.
-    // 2. Deletes and updates will be performed on already present keys.
-    // 3. additional_initial_capacity represents the amount of items already present
-    //     in the DB in addition to the ones required to perform deletes and updates.
-    // 4. Size represents the total number of operations, where reads, writes, etc
-    //     are numbers that need to sum to 100 and represent a percentage of the total size.
-    pub fn new(
-        reads: u8,
-        writes: u8,
-        deletes: u8,
-        updates: u8,
-        size: u64,
-        additional_initial_capacity: u64,
-    ) -> Result<Self> {
-        if reads + writes + deletes + updates != 100 {
-            anyhow::bail!("Operations (reads, writes, deletes, updates) must sum to 100");
-        }
+    let from_percentage = |p: u8| (size as f64 * p as f64 / 100.0) as u64;
+    let n_reads = from_percentage(reads);
+    let n_writes = from_percentage(writes);
+    let n_deletes = from_percentage(deletes);
+    let n_updates = from_percentage(updates);
 
-        let from_percentage = |p: u8| (size as f64 * p as f64 / 100.0) as u64;
-        let n_reads = from_percentage(reads);
-        let n_writes = from_percentage(writes);
-        let n_deletes = from_percentage(deletes);
-        let n_updates = from_percentage(updates);
-
-        let n_required_key = n_deletes + n_updates;
-        let initial_writes = (0..n_required_key + additional_initial_capacity)
-            .map(|i| Action::Write {
-                key: i.to_be_bytes().to_vec(),
-                value: Some(vec![64u8; 32]),
-            })
-            .collect();
-
-        let read_actions = (0..n_reads).map(|i| Action::Read { key: rand_key(i) });
-        let write_actions = (n_reads..n_reads + n_writes).map(|i| Action::Write {
-            key: rand_key(i),
-            value: Some(vec![8; 16]),
-        });
-
-        let delete_actions = (0..n_deletes).map(|i| Action::Write {
+    let n_required_key = n_deletes + n_updates;
+    let initial_writes = (0..n_required_key + additional_initial_capacity)
+        .map(|i| Action::Write {
             key: i.to_be_bytes().to_vec(),
-            value: None,
-        });
-        let update_actions = (n_deletes..n_deletes + n_updates).map(|i| Action::Write {
-            key: i.to_be_bytes().to_vec(),
-            value: Some(vec![2; 16]),
-        });
-
-        let custom_actions = read_actions
-            .chain(write_actions)
-            .chain(delete_actions)
-            .chain(update_actions)
-            .collect();
-
-        Ok(Self {
-            init_actions: initial_writes,
-            run_actions: custom_actions,
+            value: Some(vec![64u8; 32]),
         })
-    }
-}
+        .collect();
 
-impl Workload for CustomWorkload {
-    fn init(&self, backend: &mut Box<dyn Db>) {
-        backend.apply_actions(self.init_actions.clone(), None);
-    }
-    fn run(&self, backend: &mut Box<dyn Db>, timer: &mut Timer) {
-        backend.apply_actions(self.run_actions.clone(), Some(timer));
-    }
+    let read_actions = (0..n_reads).map(|i| Action::Read { key: rand_key(i) });
+    let write_actions = (n_reads..n_reads + n_writes).map(|i| Action::Write {
+        key: rand_key(i),
+        value: Some(vec![8; 16]),
+    });
+
+    let delete_actions = (0..n_deletes).map(|i| Action::Write {
+        key: i.to_be_bytes().to_vec(),
+        value: None,
+    });
+    let update_actions = (n_deletes..n_deletes + n_updates).map(|i| Action::Write {
+        key: i.to_be_bytes().to_vec(),
+        value: Some(vec![2; 16]),
+    });
+
+    let custom_actions = read_actions
+        .chain(write_actions)
+        .chain(delete_actions)
+        .chain(update_actions)
+        .collect();
+
+    Ok(Workload {
+        init_actions: initial_writes,
+        run_actions: custom_actions,
+    })
 }
 
 fn rand_key(id: u64) -> Vec<u8> {
--- xtask/src/workload.rs
+++ xtask/src/workload.rs
@@ -1,5 +1,8 @@
 use crate::{
-    backend::Db, custom_workload::CustomWorkload, timer::Timer, transfer_workload::TransferWorkload,
+    backend::{Action, Db},
+    custom_workload::new_custom_workload,
+    timer::Timer,
+    transfer_workload::new_transfer_workload,
 };
 use anyhow::Result;
 
@@ -13,9 +16,19 @@ use anyhow::Result;
 //
 // Each workload will set up the DB differently and reads and writes arbitrarily,
 // whether the key is not present or already present.
-pub trait Workload {
-    fn init(&self, backend: &mut Box<dyn Db>);
-    fn run(&self, backend: &mut Box<dyn Db>, timer: &mut Timer);
+pub struct Workload {
+    pub init_actions: Vec<Action>,
+    pub run_actions: Vec<Action>,
+}
+
+impl Workload {
+    pub fn init(&self, backend: &mut Box<dyn Db>) {
+        backend.apply_actions(self.init_actions.clone(), None);
+    }
+
+    pub fn run(&self, backend: &mut Box<dyn Db>, timer: &mut Timer) {
+        backend.apply_actions(self.run_actions.clone(), Some(timer));
+    }
 }
 
 pub fn parse(
@@ -23,45 +36,45 @@ pub fn parse(
     size: u64,
     additional_initial_capacity: u64,
     percentage_cold_transfer: Option<u8>,
-) -> Result<Box<dyn Workload>> {
+) -> Result<Workload> {
     Ok(match name {
-        "transfer" => Box::new(TransferWorkload::new(
+        "transfer" => new_transfer_workload(
             size,
             percentage_cold_transfer.unwrap_or(0),
             additional_initial_capacity,
-        )),
-        "set_balance" | "heavy_write" => Box::new(CustomWorkload::new(
+        ),
+        "set_balance" | "heavy_write" => new_custom_workload(
             0,   // reads
             100, // writes
             0,   // deletes
             0,   // updates
             size,
             additional_initial_capacity,
-        )?),
-        "heavy_read" => Box::new(CustomWorkload::new(
+        )?,
+        "heavy_read" => new_custom_workload(
             90, // reads
             10, // writes
             0,  // deletes
             0,  // updates
             size,
             additional_initial_capacity,
-        )?),
-        "heavy_update" => Box::new(CustomWorkload::new(
+        )?,
+        "heavy_update" => new_custom_workload(
             5,  // reads
             5,  // writes
             0,  // deletes
             90, // updates
             size,
             additional_initial_capacity,
-        )?),
-        "heavy_delete" => Box::new(CustomWorkload::new(
+        )?,
+        "heavy_delete" => new_custom_workload(
             5,  // reads
             5,  // writes
             90, // deletes
             0,  // updates
             size,
             additional_initial_capacity,
-        )?),
+        )?,
         name => anyhow::bail!("invalid workload name: {}", name),
     })
 }
