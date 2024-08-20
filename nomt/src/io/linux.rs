f02dac23f6c08a8aa395ec3145283f736495ca95
gabriele-0201 <gabriele.miotti2@gmail.com>
2024-08-20 18:42:13 +0200

    io: retry read/write until complete

--- nomt/src/io/linux.rs
+++ nomt/src/io/linux.rs
@@ -1,8 +1,11 @@
-use super::{CompleteIo, IoCommand, IoKind, IoPacket, PAGE_SIZE};
+use super::{CompleteIo, IoCommand, IoKind, IoKindResult, IoPacket, PAGE_SIZE};
 use crossbeam_channel::{Receiver, Sender, TryRecvError};
 use io_uring::{cqueue, opcode, squeue, types, IoUring};
 use slab::Slab;
-use std::time::{Duration, Instant};
+use std::{
+    collections::VecDeque,
+    time::{Duration, Instant},
+};
 
 const RING_CAPACITY: u32 = 128;
 
@@ -65,6 +68,7 @@ fn run_worker(command_rx: Receiver<IoPacket>) {
 
     let (submitter, mut submit_queue, mut complete_queue) = ring.split();
     let mut stats = Stats::new();
+    let mut retries = VecDeque::<IoPacket>::new();
 
     loop {
         stats.log();
@@ -84,11 +88,25 @@ fn run_worker(command_rx: Receiver<IoPacket>) {
 
                 stats.note_completion(start.elapsed().as_micros() as u64);
 
-                let result = if completion_event.result() == -1 {
-                    Err(std::io::Error::from_raw_os_error(completion_event.result()))
-                } else {
-                    Ok(())
+                // io_uring never uses errno to pass back error information.
+                // Instead, completion_event.result() will contain what the equivalent
+                // system call would have returned in case of success,
+                // and in case of error completion_event.result() will contain -errno
+                let io_uring_res = completion_event.result();
+                let syscall_result = if io_uring_res >= 0 { io_uring_res } else { -1 };
+
+                let result = match command.kind.get_result(syscall_result) {
+                    IoKindResult::Ok => Ok(()),
+                    IoKindResult::Err => Err(std::io::Error::from_raw_os_error(io_uring_res.abs())),
+                    IoKindResult::Retry => {
+                        retries.push_back(IoPacket {
+                            command,
+                            completion_sender,
+                        });
+                        continue;
+                    }
                 };
+
                 let complete = CompleteIo { command, result };
 
                 if let Err(_) = completion_sender.send(complete) {
@@ -103,7 +121,11 @@ fn run_worker(command_rx: Receiver<IoPacket>) {
 
         submit_queue.sync();
         while pending.len() < MAX_IN_FLIGHT && !submit_queue.is_full() {
-            let next_io = if pending.is_empty() {
+            let next_io = if !retries.is_empty() {
+                // re-apply partially failed reads and writes
+                // unwrap: known not empty
+                retries.pop_front().unwrap()
+            } else if pending.is_empty() {
                 // block on new I/O if nothing in-flight.
                 match command_rx.recv() {
                     Ok(command) => command,
--- nomt/src/io/unix.rs
+++ nomt/src/io/unix.rs
@@ -31,38 +31,40 @@ fn spawn_worker_thread(command_rx: Receiver<IoPacket>) {
 }
 
 fn execute(mut command: IoCommand) -> CompleteIo {
-    let err = match command.kind {
-        IoKind::Read(fd, page_index, ref mut page) => unsafe {
-            libc::pread(
-                fd,
-                page.as_mut_ptr() as *mut libc::c_void,
-                PAGE_SIZE as libc::size_t,
-                (page_index * PAGE_SIZE as u64) as libc::off_t,
-            ) == -1
-        },
-        IoKind::Write(fd, page_index, ref page) => unsafe {
-            libc::pwrite(
-                fd,
-                page.as_ptr() as *const libc::c_void,
-                PAGE_SIZE as libc::size_t,
-                (page_index * PAGE_SIZE as u64) as libc::off_t,
-            ) == -1
-        },
-        IoKind::WriteRaw(fd, page_index, ptr, size) => unsafe {
-            libc::pwrite(
-                fd,
-                ptr as *const libc::c_void,
-                size as libc::size_t,
-                (page_index * PAGE_SIZE as u64) as libc::off_t,
-            ) == -1
-        },
-        IoKind::Fsync(fd) => unsafe { libc::fsync(fd) == -1 },
-    };
+    let result = loop {
+        let res = match command.kind {
+            IoKind::Read(fd, page_index, ref mut page) => unsafe {
+                libc::pread(
+                    fd,
+                    page.as_mut_ptr() as *mut libc::c_void,
+                    PAGE_SIZE as libc::size_t,
+                    (page_index * PAGE_SIZE as u64) as libc::off_t,
+                )
+            },
+            IoKind::Write(fd, page_index, ref page) => unsafe {
+                libc::pwrite(
+                    fd,
+                    page.as_ptr() as *const libc::c_void,
+                    PAGE_SIZE as libc::size_t,
+                    (page_index * PAGE_SIZE as u64) as libc::off_t,
+                )
+            },
+            IoKind::WriteRaw(fd, page_index, ptr, size) => unsafe {
+                libc::pwrite(
+                    fd,
+                    ptr as *const libc::c_void,
+                    size as libc::size_t,
+                    (page_index * PAGE_SIZE as u64) as libc::off_t,
+                )
+            },
+            IoKind::Fsync(fd) => unsafe { libc::fsync(fd) },
+        };
 
-    let result = if err {
-        Err(std::io::Error::last_os_error())
-    } else {
-        Ok(())
+        match command.kind.get_result(res) {
+            IoKindResult::Ok => break Ok(()),
+            IoKindResult::Err => break Err(std::io::Error::last_os_error()),
+            _ => (),
+        }
     };
 
     CompleteIo { command, result }
