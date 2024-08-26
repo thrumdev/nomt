0a1808bcf63dd3583c802f814aebece4184bde05
gabriele-0201 <gabriele.miotti2@gmail.com>
2024-08-26 14:45:17 +0200

    init beatree benchmarks

---
+++ nomt/benches/beatree.rs
@@ -0,0 +1,8 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn beatreee_benchmark(_c: &mut Criterion) {
+    todo!()
+}
+
+criterion_group!(benches, beatreee_benchmark);
+criterion_main!(benches);
