5c9bc456ffe830445db562e7606127a2052d0a7a
Sergei Shulepov <s.pepyakin@gmail.com>
2024-08-26 17:10:13 +0200

    feat: WAL recovery
    This implements WAL recovery.

--- nomt/src/bitbox/wal/mod.rs
+++ nomt/src/bitbox/wal/mod.rs
@@ -1,3 +1,5 @@
+use anyhow::bail;
+
 use crate::io::PAGE_SIZE;
 use std::sync::Arc;
 
@@ -33,6 +35,9 @@ impl Drop for Mmap {
     }
 }
 
+const WAL_ENTRY_TAG_CLEAR: u8 = 1;
+const WAL_ENTRY_TAG_UPDATE: u8 = 2;
+
 /// A builder for a WAL blob.
 pub struct WalBlobBuilder {
     mmap: Arc<Mmap>,
@@ -58,7 +63,7 @@ impl WalBlobBuilder {
 
     pub fn write_clear(&mut self, bucket_index: u64) {
         unsafe {
-            self.write_byte(1);
+            self.write_byte(WAL_ENTRY_TAG_CLEAR);
             self.write(&bucket_index.to_le_bytes());
         }
     }
@@ -72,7 +77,7 @@ impl WalBlobBuilder {
     ) {
         unsafe {
             // SAFETY: Those do not overlap with the mmap.
-            self.write_byte(2);
+            self.write_byte(WAL_ENTRY_TAG_UPDATE);
             self.write(&page_id);
             self.write(&page_diff);
             for changed in changed {
@@ -147,3 +152,83 @@ impl WalBlobBuilder {
 }
 
 unsafe impl Send for WalBlobBuilder {}
+
+#[derive(Debug)]
+pub enum WalEntry {
+    Update {
+        /// The unique identifier of the page being updated.
+        page_id: [u8; 32],
+        /// A bitmap where each bit indicates whether the node at the corresponding index was
+        /// changed by this entry.
+        page_diff: [u8; 16],
+        /// The length of this array is the same as the count of ones in `page_diff`.
+        changed_nodes: Vec<[u8; 32]>,
+        /// The bucket index which is being updated.
+        bucket: u64,
+    },
+    Clear {
+        /// The bucket index which is being cleared.
+        bucket: u64,
+    },
+}
+
+pub struct WalBlobReader<'a> {
+    wal_fd: &'a std::fs::File,
+}
+impl<'a> WalBlobReader<'a> {
+    /// Creates a new WAL blob reader.
+    ///
+    /// The `wal_fd` is expected to be positioned at the start of the WAL file. The file must be
+    /// a multiple of the page size.
+    pub(crate) fn new(wal_fd: &'a std::fs::File) -> anyhow::Result<Self> {
+        let stat = wal_fd.metadata()?;
+        if stat.len() % PAGE_SIZE as u64 != 0 {
+            anyhow::bail!("WAL file size is not a multiple of the page size");
+        }
+        Ok(Self { wal_fd })
+    }
+
+    /// Reads the next entry from the WAL file.
+    ///
+    /// Returns `None` if the end of the file is reached.
+    pub fn read_entry(&mut self) -> anyhow::Result<Option<WalEntry>> {
+        use crate::io::Page;
+        use std::io::Read;
+
+        let mut buffer = Box::new(Page::zeroed());
+        self.wal_fd.read_exact(&mut buffer[..])?;
+
+        let entry_tag = buffer[0];
+        match entry_tag {
+            WAL_ENTRY_TAG_CLEAR => {
+                let bucket = u64::from_le_bytes(buffer[1..9].try_into().unwrap());
+                Ok(Some(WalEntry::Clear { bucket }))
+            }
+            WAL_ENTRY_TAG_UPDATE => {
+                let page_id: [u8; 32] = buffer[1..33].try_into().unwrap();
+                let page_diff: [u8; 16] = buffer[33..49].try_into().unwrap();
+
+                // Calculate the number of changed nodes based on the page_diff
+                let changed_count =
+                    page_diff.iter().map(|&byte| byte.count_ones()).sum::<u32>() as usize;
+
+                let mut changed_nodes = Vec::with_capacity(changed_count);
+                let mut offset = 49;
+                for _ in 0..changed_count {
+                    changed_nodes.push(buffer[offset..offset + 32].try_into().unwrap());
+                    offset += 32;
+                }
+
+                let bucket = u64::from_le_bytes(buffer[offset..offset + 8].try_into().unwrap());
+
+                Ok(Some(WalEntry::Update {
+                    page_id,
+                    page_diff,
+                    changed_nodes,
+                    bucket,
+                }))
+            }
+            _ => bail!("unknown WAL entry tag: {entry_tag}"),
+        }
+    }
+}
