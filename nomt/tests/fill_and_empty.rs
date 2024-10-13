6a83df55b7030832a2936a06001ddbeebc9abcbe
gabriele-0201 <gabriele.miotti2@gmail.com>
2024-10-13 10:16:55 +0200

    reproduce hashtable bug

--- nomt/tests/fill_and_empty.rs
+++ nomt/tests/fill_and_empty.rs
@@ -4,67 +4,72 @@ use rand::{prelude::SliceRandom, Rng, SeedableRng};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn fill_and_empty(commit_concurrency: usize) {
-    let seed = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("no time?")
-        .as_nanos()
-        .to_le_bytes()[0..16]
-        .try_into()
-        .unwrap();
+    loop {
+        let seed = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("no time?")
+            .as_nanos()
+            .to_le_bytes()[0..16]
+            .try_into()
+            .unwrap();
 
-    let mut rng = rand_pcg::Lcg64Xsh32::from_seed(seed);
+        println!("test seed: {:?}", seed);
+        let mut rng = rand_pcg::Pcg32::from_seed(seed);
 
-    let db_size = 1 << 12;
-    let commit_size = db_size / 16;
+        let db_size = 1 << 16;
+        let commit_size = db_size / 16;
 
-    let mut items = std::collections::HashSet::new();
-    while items.len() < db_size as usize {
-        items.insert(rand_key(&mut rng));
-    }
-    let items: Vec<_> = items.into_iter().collect();
+        let mut items = std::collections::BTreeSet::new();
+        while items.len() < db_size as usize {
+            items.insert(rand_key(&mut rng));
+        }
+        let items: Vec<_> = items.into_iter().collect();
 
-    let mut to_delete: Vec<usize> = (0..db_size as usize).collect();
-    to_delete.shuffle(&mut rng);
+        let mut to_insert: Vec<usize> = (0..db_size as usize).collect();
+        to_insert.shuffle(&mut rng);
+        let mut to_delete = to_insert.clone();
+        to_delete.shuffle(&mut rng);
 
-    let mut t = Test::new_with_params(
-        format!("fill_and_empty_{}", commit_concurrency), // name
-        commit_concurrency,
-        15000, // hashtable_buckets
-        false, // panic_on_sync
-        true,  //  cleanup_dir
-    );
+        let mut t = Test::new_with_params(
+            format!("fill_and_empty_{}", commit_concurrency), // name
+            commit_concurrency,
+            1_000_000, // hashtable_buckets
+            false,     // panic_on_sync
+            true,      // cleanup_dir
+        );
 
-    // inserting all the values
-    let mut to_check = vec![];
-    for i in 0..db_size {
-        let key = items[i];
-        let value = vec![i as u8; 400];
+        // inserting all the values
+        //let mut to_check = vec![];
+        for i in 0..db_size {
+            let key = items[to_insert[i]];
+            let value = vec![i as u8; 400];
 
-        to_check.push((key, value.clone()));
-        t.write(key, Some(value));
+            //to_check.push((key, value.clone()));
+            t.write(key, Some(value));
 
-        if i != 0 && i % commit_size == 0 {
-            t.commit();
-            // check for presence
-            for (key, value) in to_check.drain(..) {
-                assert_eq!(t.read(key), Some(value));
+            if i != 0 && i % commit_size == 0 {
+                t.commit();
+                // check for presence
+                //for (key, value) in to_check.drain(..) {
+                //assert_eq!(t.read(key), Some(value));
+                //}
             }
         }
-    }
 
-    // deleting all the values in different order
-    let mut to_check = vec![];
-    for i in 0..db_size {
-        let key = items[to_delete[i]];
+        // deleting all the values in different order
+        //let mut to_check = vec![];
+        for i in 0..db_size {
+            let key = items[to_delete[i]];
 
-        to_check.push(key);
-        t.write(key, None);
+            //to_check.push(key);
+            t.write(key, None);
 
-        if i != 0 && i % commit_size == 0 {
-            t.commit();
-            // check for absence
-            for key in to_check.drain(..) {
-                assert_eq!(t.read(key), None);
+            if i != 0 && i % commit_size == 0 {
+                t.commit();
+                // check for absence
+                //for key in to_check.drain(..) {
+                //assert_eq!(t.read(key), None);
+                //}
             }
         }
     }
