49b7be02847b29579b0ba824599bbb2369945f59
Robert Habermeier <rphmeier@gmail.com>
2024-11-05 15:40:16 -0600

    stop RA warning on nomt/benches

--- nomt/benches/beatree.rs
+++ nomt/benches/beatree.rs
@@ -1,7 +1,12 @@
-#![cfg(feature = "benchmarks")]
-
+#[cfg(feature = "benchmarks")]
 use criterion::{criterion_group, criterion_main};
+#[cfg(feature = "benchmarks")]
 use nomt::beatree::benches::beatree_benchmark;
 
+#[cfg(feature = "benchmarks")]
 criterion_group!(benches, beatree_benchmark);
+#[cfg(feature = "benchmarks")]
 criterion_main!(benches);
+
+#[cfg(not(feature = "benchmarks"))]
+fn main() {}
