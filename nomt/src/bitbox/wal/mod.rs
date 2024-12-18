c456ebfbd3c37edb57ec8afb2fcbb5bcebf2cdab
Robert Habermeier <rphmeier@gmail.com>
2024-12-18 16:42:20 -0600

    refactor: change all WAL tags to be non-zero

--- nomt/src/bitbox/wal/mod.rs
+++ nomt/src/bitbox/wal/mod.rs
@@ -1,7 +1,7 @@
-const WAL_ENTRY_TAG_START: u8 = 0;
-const WAL_ENTRY_TAG_END: u8 = 1;
-const WAL_ENTRY_TAG_CLEAR: u8 = 2;
-const WAL_ENTRY_TAG_UPDATE: u8 = 3;
+const WAL_ENTRY_TAG_START: u8 = 1;
+const WAL_ENTRY_TAG_END: u8 = 2;
+const WAL_ENTRY_TAG_CLEAR: u8 = 3;
+const WAL_ENTRY_TAG_UPDATE: u8 = 4;
 
 pub use read::{WalBlobReader, WalEntry};
 pub use write::WalBlobBuilder;
