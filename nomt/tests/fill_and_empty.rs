f13c3fea262e7784ee8533f9a5430892858ae675
gabriele-0201 <gabriele.miotti2@gmail.com>
2025-01-09 14:20:31 +0100

    test: fix fill_and_empty
    make fill_and_empty reproducible and effectively
    write and read all generated keys

--- nomt/tests/fill_and_empty.rs
+++ nomt/tests/fill_and_empty.rs
@@ -3,25 +3,28 @@ use common::Test;
 use rand::{prelude::SliceRandom, Rng, SeedableRng};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-fn fill_and_empty(commit_concurrency: usize) {
-    let seed = SystemTime::now()
+fn seed() -> [u8; 16] {
+    SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("no time?")
         .as_nanos()
         .to_le_bytes()[0..16]
         .try_into()
-        .unwrap();
+        .unwrap()
+}
 
+fn fill_and_empty(seed: [u8; 16], commit_concurrency: usize) {
     let mut rng = rand_pcg::Lcg64Xsh32::from_seed(seed);
 
     let db_size = 1 << 12;
     let commit_size = db_size / 16;
 
-    let mut items = std::collections::HashSet::new();
+    let mut items = std::collections::BTreeSet::new();
     while items.len() < db_size as usize {
         items.insert(rand_key(&mut rng));
     }
-    let items: Vec<_> = items.into_iter().collect();
+    let mut items: Vec<_> = items.into_iter().collect();
+    items.shuffle(&mut rng);
 
     let mut to_delete: Vec<usize> = (0..db_size as usize).collect();
     to_delete.shuffle(&mut rng);
@@ -43,7 +46,7 @@ fn fill_and_empty(commit_concurrency: usize) {
         to_check.push((key, value.clone()));
         t.write(key, Some(value));
 
-        if i != 0 && i % commit_size == 0 {
+        if (i + 1) % commit_size == 0 {
             t.commit();
             // check for presence
             for (key, value) in to_check.drain(..) {
@@ -60,7 +63,7 @@ fn fill_and_empty(commit_concurrency: usize) {
         to_check.push(key);
         t.write(key, None);
 
-        if i != 0 && i % commit_size == 0 {
+        if (i + 1) % commit_size == 0 {
             t.commit();
             // check for absence
             for key in to_check.drain(..) {
@@ -68,6 +71,8 @@ fn fill_and_empty(commit_concurrency: usize) {
             }
         }
     }
+
+    assert_eq!([0; 32], t.commit().0);
 }
 
 fn rand_key(rng: &mut impl Rng) -> [u8; 32] {
@@ -78,10 +83,30 @@ fn rand_key(rng: &mut impl Rng) -> [u8; 32] {
 
 #[test]
 fn fill_and_empty_1_commit_worker() {
-    fill_and_empty(1);
+    let seed = seed();
+    let test_result = std::panic::catch_unwind(|| {
+        fill_and_empty(seed, 1);
+    });
+    if let Err(cause) = test_result {
+        eprintln!(
+            "fill_and_empty_1_commit_worker failed with seed: {:?}",
+            seed
+        );
+        std::panic::resume_unwind(cause);
+    }
 }
 
 #[test]
 fn fill_and_empty_64_commit_worker() {
-    fill_and_empty(64);
+    let seed = seed();
+    let test_result = std::panic::catch_unwind(|| {
+        fill_and_empty(seed, 64);
+    });
+    if let Err(cause) = test_result {
+        eprintln!(
+            "fill_and_empty_64_commit_worker failed with seed: {:?}",
+            seed
+        );
+        std::panic::resume_unwind(cause);
+    }
 }
