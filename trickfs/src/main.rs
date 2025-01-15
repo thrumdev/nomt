cc46eda7ab4b2cd5a0b27bbd28f3dc5905929229
Sergei Shulepov <s.pepyakin@gmail.com>
2025-01-15 13:56:25 +0000

    trickfs: add trickmnt
    adds a binary target called `trickmnt` to quickly mount a trickfs
    locally.

--- trickfs/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let _ = env_logger::builder()
-        .filter_level(log::LevelFilter::Trace)
-        .init();
-    let handle = trickfs::spawn_trick("/tmp/trick").unwrap();
-    println!("running...");
-    std::io::stdin().read_line(&mut String::new()).unwrap();
-    drop(handle);
-}
