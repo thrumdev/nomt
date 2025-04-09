c590d405325843f538b18dcb76e6b0383a8f8f94
Nikolai Golub <nikolai@sovlabs.io>
2025-04-09 11:33:25 +0200

    Slightly better commit message

--- nomt/src/lib.rs
+++ nomt/src/lib.rs
@@ -577,7 +577,7 @@ impl<T: HashAlgorithm> Session<T> {
         self.store.load_value(path)
     }
 
-    /// Prev root
+    /// Previous root
     pub fn prev_root(&self) -> Root {
         self.prev_root
     }
